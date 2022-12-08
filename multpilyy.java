cee9ef8267a613c74cfee0cac2cfa5fb7cd2f115
Ayan Mohammad <mohammadayan1002@gmail.com>
2022-12-08 22:30:27 +0530

    added multiply.java

---
+++ multpilyy.java
@@ -0,0 +1,9 @@
+public class multpilyy {
+    
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=20;
+        System.out.println("The product is "+(a*b));
+    }
+}
