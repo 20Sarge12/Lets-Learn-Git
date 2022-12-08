14d1f0c15089e541199f8d78118690864538e6b6
Ayan Mohammad <mohammadayan1002@gmail.com>
2022-12-08 14:38:43 +0530

    adding diff.java

--- diff.java
+++ diff.java
@@ -1,10 +1,8 @@
-public class Sum
-{
+public class diff {
     public static void main(String args[])
     {
         int a=10;
         int b=20;
-        System.out.println("The sum is "+(a+b));
+        System.out.println("The difference is "+(a-b));
     }
-
 }
